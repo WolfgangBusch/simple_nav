3e0f8faa86743b80aef98e6a1ad2e31a00999d1d
Wolfgang Busch <wolfgang@busch-dettum.de>
2020-09-29 13:52:11 +0200

    Add files via upload

--- pages/settings.php
+++ pages/settings.php
@@ -3,7 +3,7 @@
  * simple Navigation AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version März 2020
+ * @version September 2020
  */
 simple_nav_config::config();
 ?>
